a970c7dd9819405ee45cb4fa140e7673e6b13362
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-21 02:23:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,23 @@
 # Add  code here!
 def prime?(num)
-  if num <= 1
-    false
-  elsif num == 2
-    true
-  else
-    (2..num/2).none? { |i| num % i == 0}
+  #if num <= 1
+  #  false
+  #elsif num == 2
+  #  true
+  #else
+  #  (2..num/2).none? { |i| num % i == 0}
+  #end
+
+
+  (2..num).each do |n|
+    if n <= 1
+      false
+    elsif num/2 == 0
+      false
+    else
+      true
+    end
   end
-
-
-  #(2..num).each do |n|
-
-  #  if num/n == 0 || num < 0
-  #
+      
+  
 end
